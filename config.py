0f4c2190e4becb31d030eb7471138b3d763472e2
Derek Zhao <chong.zhao@columbia.edu>
2019-03-30 10:29:18 -0700

    add project path configs

---
+++ config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+PROJECT_DIR = Path(__file__).parent.resolve()
+
+APP_DIR = PROJECT_DIR / 'app'
+DATA_DIR = PROJECT_DIR / 'data'
+EXPERIMENTS_DIR = PROJECT_DIR / 'experiments'
+LIBRARY_DIR = PROJECT_DIR / 'moisturize'
+NOTEBOOKS_DIR = PROJECT_DIR / 'notebooks'
+
+CELEB_A_DIR = DATA_DIR / 'celeb-a.nosync'
