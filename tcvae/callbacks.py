4dace8aee11479e2a59d51649adf91d1d0b4e748
Derek Zhao <derek.zhao@bhge.com>
2019-04-09 17:13:37 -0700

    restore callbacks.py

--- tcvae/callbacks.py
+++ tcvae/callbacks.py
@@ -4,16 +4,16 @@
 This module contains custom callback functions to be used for model training.
 """
 
+import csv
 from pathlib import Path
 
 import imageio
 import numpy as np
 from keras.callbacks import Callback
 from tcvae.utils import check_path, make_directory
-from tcvae.models import _make_autoencoder_models
+from tcvae.models import _make_autoencoder_models, TCVAE
 
 
-# TODO: reconstruction check callback
 class ReconstructionCheck(Callback):
     """
     Parameters
@@ -22,29 +22,33 @@ class ReconstructionCheck(Callback):
         A batch of images to be reconstructed with shape (num_images,
         img_height, img_width, num_channels)
     output_dir : str or pathlib.Path
+        The directory in which reconstructed images are stored.
+    stem : str
+        The name of the saved image files (without file extension).
+    fmt : str
+        One of {`jpg`, `png`}. Determines the extension of the saved image
+        files.
     """
 
     def __init__(
-            self, images, output_dir, stem='reconstruction', format='jpg'):
+            self, images, output_dir, stem='reconstruction', fmt='jpg'):
         super().__init__()
         self.images = images
         self.output_dir = check_path(output_dir, Path)
         self.stem = stem
-        if format[0] != '.':
-            format = '.' + format
-        self.format = format
+        if fmt[0] != '.':
+            fmt = '.' + fmt
+        self.format = fmt
         self.model_predict = None
 
     def on_train_begin(self, logs=None):
         make_directory(self.output_dir, overwrite=True)
+        encoder = self.model.get_layer('encoder')
+        decoder = self.model.get_layer('decoder')
+        _, self.model_predict, _ = _make_autoencoder_models(
+            encoder, decoder)
 
     def on_epoch_end(self, epoch, logs=None):
-        # Construct prediction model
-        if self.model_predict is None:
-            encoder = self.model.get_layer('encoder')
-            decoder = self.model.get_layer('decoder')
-            _, self.model_predict, _ = _make_autoencoder_models(
-                encoder, decoder)
         # Make reconstructions and stitch with original image
         reconstructions = self.model_predict.predict(self.images)
         enum_zip = enumerate(zip(self.images, reconstructions))
@@ -55,6 +59,104 @@ class ReconstructionCheck(Callback):
             imageio.imwrite(save_file.as_posix(), img_pair)
 
 
-# TODO: latent traversal check callback
+class LatentTraversalCheck(Callback):
+    """
+    Parameters
+    ----------
+    image : np.ndarray
+        An image on which to perform traversals along all latent dimensions.
+    output_dir : str or pathlib.Path
+        The directory in which traversal animations are stored.
+    stem : str
+        The name of the saved image files (without file extension).
+    """
+    def __init__(
+            self, image, output_dir, stem='traversal', traversal_range=(-4, 4),
+            traversal_resolution=10):
+        super().__init__()
+        if image.ndim == 4:
+            assert(image.shape[0] == 1), (
+                'Traversal check can only be performed on a single image.')
+        else:
+            image = [image]
+        self.image = image
+        self.output_dir = check_path(output_dir, Path)
+        self.stem = stem
+        self.tcvae = None
+        self.traversal_range = traversal_range
+        self.traversal_resolution = traversal_resolution
 
-# TODO: latent distribution monitoring callback
+    def on_train_begin(self, logs=None):
+        make_directory(self.output_dir, overwrite=False)
+        encoder = self.model.get_layer('encoder')
+        decoder = self.model.get_layer('decoder')
+        self.tcvae = TCVAE(encoder, decoder)
+
+    def on_epoch_end(self, epoch, logs=None):
+        traversals = self.tcvae.make_all_traversals(
+            self.image, self.traversal_range, self.traversal_resolution,
+            std_threshold=None, output_format='traversals_first')
+        traversals = traversals.squeeze()  # Remove image sample dimension
+        for index, traversal in enumerate(traversals):
+            base = '{}-{:0>2}.gif'.format(self.stem, index)
+            save_file = self.output_dir / base
+            imageio.mimwrite(save_file.as_posix(), traversal)
+
+
+class LatentDistributionLogging(Callback):
+    """
+    Parameters
+    ----------
+    csv_file : str or pathlib.Path
+        The path of the CSV file where the average latent means and standard
+        distributions are logged following each training epoch.
+    data : np.ndarray or tcvae.data.ImageDataGenerator
+        A batch of images whose latent distributions are averaged following
+        each training epoch.
+    """
+
+    def __init__(self, csv_file, data, batch_size=512):
+        super().__init__()
+        self.csv_file = check_path(csv_file, path_type=str)
+        self.data = data
+        self.batch_size = batch_size
+
+        self.encoder = None
+        self.num_latents = None
+        self.csv_header = None
+        self.batch_means = None
+        self.batch_stds = None
+
+    def on_train_begin(self):
+        self.encoder = self.model.get_layer('encoder')
+        z, z_mu, z_log_sigma = self.encoder.outputs
+        self.num_latents = int(z.shape[-1])
+        if not self.csv_file.is_file():
+            mean_header = [
+                'z_mu_{:0>2}'.format(i) for i in range(self.num_latents)]
+            std_header = [
+                'z_std_{:0>2}'.format(i) for i in range(self.num_latents)]
+            csv_header = mean_header + std_header
+            with open(self.csv_file.as_posix(), 'at') as f:
+                reader = csv.reader(f)
+                reader.writerow(csv_header)
+
+    def on_epoch_end(self, epoch, logs=None):
+        if isinstance(self.data, np.ndarray):
+            _, z_mu, z_log_sigma = self.encoder.predict(
+                self.data, self.data, batch_size=self.batch_size)
+        elif isinstance(self.data, np.ndarray):
+            _, z_mu, z_log_sigma = self.encoder.predict_generator(
+                self.data, batch_size=self.batch_size)
+        else:
+            raise ValueError(
+                'Argument for parameter `data` must be a Numpy array or '
+                'ImageDataGenerator.')
+        z_sigma = np.exp(z_log_sigma)
+        z_mu_epoch = z_mu.mean(axis=0).tolist()
+        z_sigma_epoch = z_sigma.mean(axis=0).tolist()
+        row = z_mu_epoch + z_sigma_epoch
+        with open(self.csv_file.as_posix(), 'at') as f:
+            reader = csv.reader(f)
+            reader.writerow(row)
+
