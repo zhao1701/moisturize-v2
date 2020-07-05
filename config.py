323e72f1318912686c37487f1f4374df3a123910
Zhao, Chong <chozhao@ea.com>
2020-07-05 15:24:13 -0500

    update exploration notebook

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
+
 from pathlib import Path
 
 
 PROJECT_DIR = Path(__file__).parent.resolve()
+
 
 APP_DIR = PROJECT_DIR / 'app'
 DATA_DIR = PROJECT_DIR / 'data'
@@ -11,6 +13,7 @@ EXPERIMENTS_DIR = PROJECT_DIR / 'experiments'
 LIBRARY_DIR = PROJECT_DIR / 'tcvae'
 NOTEBOOKS_DIR = PROJECT_DIR / 'notebooks'
 
-CELEB_A_DIR = DATA_DIR / 'celeb-a.nosync'
+
+TRAIN_DATA_DIR = DATA_DIR / 'train'
 MODELS_DIR = LIBRARY_DIR / 'models'
 YAML_DIR = EXPERIMENTS_DIR / 'yaml'
